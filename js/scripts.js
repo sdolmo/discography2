9bc2e3da70a9cca4506430781a82b9c50dfe56e3
Sylvia <sylviad1221@gmail.com>
2016-11-23 01:48:24 -0500

    get turntable animation working with trigger

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$('.trigger').on('click', function(){
+  $('.handle').toggleClass('clicked');
+});
+
+$('.trigger').on('click', function(){
+  var state = $('.record img').css('animation-play-state');
+  if (state === 'paused'){
+    $('.record img').css('animation-play-state', 'running');
+  } else if (state === 'running') {
+    $('.record img').css('animation-play-state', 'paused');
+  }
+});
